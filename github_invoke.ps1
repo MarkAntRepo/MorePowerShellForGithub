6c783abba2ab7f33929a3e2ec75d87b19accfb97
MarkAntRepo <s114119@ap.be>
2021-06-02 13:31:35 +0200

    Gets header from credentials.

---
+++ github_invoke.ps1
@@ -0,0 +1,3 @@
+﻿$auth= [System.Convert]::ToBase64String([char[]]$creds.GetNetworkCredential().Password)
+$headers = @{Authorization="Basic $auth"}
+Invoke-RestMethod -Headers $headers https://api.github.com/user
