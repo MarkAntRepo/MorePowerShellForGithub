833a63163c644bc3e71658a4aa4309804ad08f98
MarkAntRepo <s114119@ap.be>
2021-06-02 13:21:59 +0200

    Get github credential through access token for this session only.

---
+++ github_auth.ps1
@@ -0,0 +1,5 @@
+﻿Get-Content .\secret.txt| 
+ConvertTo-SecureString | Set-Variable ss_token
+$creds = New-Object pscredential 'user’, $ss_token
+Set-GitHubAuthentication -SessionOnly -Credential $creds
+
