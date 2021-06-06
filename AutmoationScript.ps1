ea38de00cc56329a114ff967afad61ec22372f25
MarkAntRepo <s114119@ap.be>
2021-06-06 16:26:49 +0200

    Autmation script making repository and adding collaborator.

---
+++ AutmoationScript.ps1
@@ -0,0 +1,22 @@
+﻿
+
+Import-Module .\MorePowerShellForGithub\more_powershell_for_github.psm1
+
+$Cred = Set-MoreGitHubAuthentication
+
+$header = Get-AuthHeader -Creds $Cred
+
+New-GitHubRepository -RepositoryName testRep -Private -AutoInit | Add-GitHubCollaborator -header $header -Collaborator 'MarkovicAntonio' | Set-Variable test
+
+$RepositoryName = ($test.repository).name
+$Login = ($test.inviter).login
+
+$FullUri = 'https://api.github.com/repos/'+ $Login +'/'+ $RepositoryName + '/zipball/master'
+    
+
+Write-Host $FullUri
+
+Invoke-RestMethod -Method Get -Headers $header -Uri $FullUri
+
+
+#Expand-Archive .\($test.repository).name
